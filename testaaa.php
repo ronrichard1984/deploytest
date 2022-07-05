f409622c27b5fa57b281dbaf20b6dfd349c6e22b
ronrichard1984 <ronrichard1984@gmail.com>
2022-07-05 13:07:51 +0600

    US-362 modified 3

--- testaaa.php
+++ testaaa.php
@@ -7,4 +7,8 @@ Update cherry-pick to show Nicolas
 
 Update cherry-pick to show Nicolas Version 2
 
+<<<<<<< HEAD
 Modified by testing branch
+=======
+Update cherry-pick to show Nicolas Version 3
+>>>>>>> 3d7976b (US-362 modified 3)
