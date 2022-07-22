5f67d1f670f685c04374613ef5a7ae684de3c5bf
ronrichard1984 <ronrichard1984@gmail.com>
2022-07-22 22:54:55 +0600

    Update testaaa.php

--- testaaa.php
+++ testaaa.php
@@ -7,8 +7,4 @@ Update cherry-pick to show Nicolas
 
 Update cherry-pick to show Nicolas Version 2
 
-<<<<<<< HEAD
-Modified by testing branch
-=======
-Update cherry-pick to show Nicolas Version 3
->>>>>>> 3d7976b (US-362 modified 3)
+Removed merge conflict
